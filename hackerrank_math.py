b10f3e52bf9709784f718f54b5646fe45ff0a9c5
zbari023 <barry450643@gmail.com>
2023-03-27 22:11:15 +0200

    new task

---
+++ hackerrank_math.py
@@ -0,0 +1,5 @@
+for i in range(1,5):
+  print(f'{i}'*i)
+print('**** other way ****')
+for j in range(1,5): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**j)//9)*j)
