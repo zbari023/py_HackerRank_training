be7e8b82ebea4efb5574948a4879db6f591e8d11
zbari023 <barry450643@gmail.com>
2023-03-27 22:28:31 +0200

    change the plan

--- class_OOP_student.py
+++ class_OOP_student.py
@@ -5,10 +5,9 @@ class Noteofstudent():
         self.name = input('Enter the name of student : ')
         self.age = int(input(f'{self.name} age : '))
         self.ID = int(input(f'{self.name} ID : '))
-        print('please Enter the Information of Student: ')
-
-        class Noteofstudent():
-            def __init__(self):
-                self.name = input('Enter the name of student : ')
-                self.age = int(input(f'{self.name} age : '))
-                self.ID = int(input(f'{self.name} ID : '))
+        print(f'The Student {self.name} is {self.age} years old and have ID: {self.ID}')
+    def listing_Notes(self):
+        self.Note_list = []                # empty list
+        print('Now you can enter his Notes and when you press 00 is to be end of your Notes :')
+        while True:
+            Note = float(input('Note: '))
