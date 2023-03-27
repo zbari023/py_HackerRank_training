30ca2b1c8421719a8c7988b369a5949ae040dc62
zbari023 <barry450643@gmail.com>
2023-03-27 22:18:59 +0200

    new work

--- hackerrank_math.py
+++ hackerrank_math.py
@@ -25,3 +25,21 @@ img = z.imag
 print(math.sqrt( (real**2 + img**2 )))
 phi =  math.atan(img / real)
 print(abs(complex(phi)) )
+
+print('**********')
+a = int(input('a= '))
+b = int(input('b= '))
+print(a//b)
+print(a%b)
+tuple_add=(a//b,a%b)
+print(tuple_add)
+'''
+def comtocer(x,y):
+    z = complex(x, y)
+    r = math.sqrt(pow(x, 2) + pow(y, 2))
+    phi = math.atan(y / x)
+    print(r)
+    print(phi)
+
+comtocer(1,2)
+'''
