755d2987930264882c0b0e955369752bc2267138
zbari023 <barry450643@gmail.com>
2023-03-27 22:22:32 +0200

    new task

---
+++ class_OOP_student.py
@@ -0,0 +1,7 @@
+print('please Enter the Information of Student: ')
+
+class Noteofstudent():
+    def __init__(self):
+        self.name = input('Enter the name of student : ')
+        self.age = int(input(f'{self.name} age : '))
+        self.ID = int(input(f'{self.name} ID : '))
