abdac3daa26defc06b03318a2b0af485087f0c05
zbari023 <barry450643@gmail.com>
2023-03-28 22:26:30 +0200

    new training

--- listes_hackerrank.py
+++ listes_hackerrank.py
@@ -1,4 +1,7 @@
-list_name =[]
-list_name.insert(0,12)
-
-print(list_name)
+# convert to list
+N = input('Enter your numbers with "," between its to convert its to list: ')
+x=N.split(',')
+l=[]
+for i in x:
+    l.append(int(i))
+print(l)
