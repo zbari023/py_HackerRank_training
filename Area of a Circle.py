48a5780997c35d74c0881881c75de970ad938601
zbari023 <barry450643@gmail.com>
2023-04-03 23:35:12 +0200

    new task

---
+++ Area of a Circle.py
@@ -0,0 +1,2 @@
+# How to find Area of a Circle in Python
+print('How to find Area of a Circle in Python ?')
---
+++ Current Username.py
@@ -0,0 +1,4 @@
+# How to Get the Current Username in Python
+print('How to Get the Current Username in Python?')
+import getpass
+print(getpass.getuser())
