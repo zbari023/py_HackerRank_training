0ddd9bab80864dc74d63130b1f0be37a87e90474
zbari023 <barry450643@gmail.com>
2023-04-03 23:42:27 +0200

    new in task

--- Area of a Circle.py
+++ Area of a Circle.py
@@ -1,2 +1,11 @@
 # How to find Area of a Circle in Python
 print('How to find Area of a Circle in Python ?')
+import math
+
+def cirArea(r):
+    if r >= 0:
+        return math.pi * pow(r, 2)
+    else:
+        print('The radius should be positiv Number')
+
+print(cirArea(1))
