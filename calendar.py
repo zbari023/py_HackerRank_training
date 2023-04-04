665e189dcadd204fcf094ef5450a12c2127e8eee
zbari023 <barry450643@gmail.com>
2023-04-04 23:06:51 +0200

    new task

---
+++ calendar.py
@@ -0,0 +1,8 @@
+import calendar
+# How to print a calendar for the given month and year in Python
+print('How to print a calendar for the given month and year in Python?')
+
+yea = input('Enter the year : ')
+mont = input('Enter the month of year: ')
+c1 = calendar.mont
+print(c1)
