cc3e32cacaf0a5f2c5d049a2772b26e6ce9d296d
zbari023 <barry450643@gmail.com>
2023-04-04 23:26:12 +0200

    new task

---
+++ Volume and Surface Area of Sphere.py
@@ -0,0 +1,7 @@
+import math
+# How to find Volume and Surface Area of Sphere in Python
+print('How to find Volume and Surface Area of Sphere in Python ?')
+def sphvol(r):
+    return (3/4) * math.pi * pow(r,3)
+print(sphvol(2))
+
--- calendar.py
+++ calendar.py
@@ -1,8 +1,6 @@
-import calendar
 # How to print a calendar for the given month and year in Python
 print('How to print a calendar for the given month and year in Python?')
-
-yea = input('Enter the year : ')
-mont = input('Enter the month of year: ')
-c1 = calendar.mont
-print(c1)
+import calendar
+year = int(input('Enter the year : '))
+mont = int(input('Enter the month of year: '))
+print(calendar.month(year,mont))
