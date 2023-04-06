a3348e7f1ba2d62e0f7c48a5cc8d94d5538e2efe
zbari023 <barry450643@gmail.com>
2023-04-06 23:52:36 +0200

    work

--- reverse a number.py
+++ reverse a number.py
@@ -1,4 +1,4 @@
 # How to reverse a number using slice operations in python
 print('How to reverse a number using slice operations in python ?')
 num = input('Enter your Number : ')
-print(f'The reverse Number is {str(num)[::-2]}')
+print(f'The reverse Number is {str(num)[::1][::-1]}')
