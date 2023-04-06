05e5d9ec46b820963c611294f6dc79c0e9d0eafe
zbari023 <barry450643@gmail.com>
2023-04-07 00:03:41 +0200

    area of triangle

---
+++ area of a triangle.py
@@ -0,0 +1,6 @@
+# How to find area of triangle given base and height in Python
+print('How to find area of triangle given base and height in Python')
+def areatr(base,height):
+    return 0.5 * base * height
+
+print(areatr(2,4))
