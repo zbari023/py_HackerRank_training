c0bc5957b549754c379633238024e7388f0a1296
zbari023 <barry450643@gmail.com>
2023-04-08 00:51:04 +0200

    filnal of task

--- string-formatting.py
+++ string-formatting.py
@@ -1,9 +1,18 @@
 # task from HackerRank
+def print_formatted(number):
+    # your code goes here
+    l = len(bin(number)[2::])
+    for i in range(1,number+1):
+        print(str(i).rjust(l),oct(i)[2::].rjust(l),hex(i)[2::].rjust(l).upper(),bin(i)[2::].rjust(l))
+
+print_formatted(4)
+'''
 def formating(num):
     for i in range(1,num+1):
         print(f"{i}\t{format(i, 'o')}\t{format(i, 'x')}\t{format(i, 'b')}")
 
 formating(1)
+'''
 '''
 num = int(input("Enter a decimal number: "))
 
