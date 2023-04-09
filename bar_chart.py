023358644686c68e3d5ae67d870a3aa2e7a39f99
zbari023 <barry450643@gmail.com>
2023-04-10 00:58:59 +0200

    new work

--- bar_chart.py
+++ bar_chart.py
@@ -1,5 +1,13 @@
 # How to Create a bar chart from a given list of integers in Python
-
+def histo(items):
+    for n in items:
+        out = ''
+        while n > 0:
+            out += '*'
+            n =n-1
+        print(out)
+histo([10,2,3,4,5])
+'''
 def histogram(listing):
     listing = listing.split(',') # darstellung der input
     l =[]                        # darstellung der neuen list
@@ -10,3 +18,4 @@ def histogram(listing):
 
 
 print(histogram('1,2,3,4,5'))
+'''
